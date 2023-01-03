d5a7a7675dba1d8340374df893a75cb809ff1a85
3barney <brnbsjm@gmail.com>
2023-01-03 18:58:40 +0300

    <your_commit_message>

--- app/server.go
+++ app/server.go
@@ -20,7 +20,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
 
 	for {
 		buffer := make([]byte, 1024)
@@ -33,10 +40,5 @@ func main() {
 			}
 		}
 		conn.Write([]byte("+PONG\r\n"))
-	}
-
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
 	}
 }
